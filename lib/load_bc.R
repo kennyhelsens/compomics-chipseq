c8f4b91c442383448ae63f6fbe74402e2e2ab6e7
Compomics <compomics@cotmp-ubuntu1004-64.(none)>
2012-07-13 15:47:12 +0200

    - created separate bioc loader for first initilization

---
+++ lib/load_bc.R
@@ -0,0 +1,7 @@
+source("http://bioconductor.org/biocLite.R")
+biocLite()
+biocLite(c("GenomicFeatures", "AnnotationDbi"))
+biocLite("bitops")
+biocLite("Rsamtools")
+biocLite("BayesPeak")
+biocLite("DiffBind")
--- lib/load_libraries.R
+++ lib/load_libraries.R
@@ -1,11 +1,5 @@
-source("http://bioconductor.org/biocLite.R")
-# biocLite("DiffBind")
-# biocLite("BayesPeak")
-biocLite("Rsamtools")
-
 library("DiffBind")
 library("BayesPeak")
 library("multicore")
 library("Rsamtools")
 
-asBam()
